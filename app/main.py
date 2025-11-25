ac5f5db7d61121df3ba5d12a27639b282ee2cfd0
miklospocsaji <miklos.pocsaji@gmail.com>
2025-11-25 11:03:48 +0100

    fixes 2

--- app/main.py
+++ app/main.py
@@ -9,27 +9,19 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
+    Person.people = {
+        person["name"]: Person(person["name"], person["age"])
+        for person in people
+    }
     for person in people:
-        # If already exists, skip creation but update age
-        if person["name"] in Person.people:
-            Person.people[person["name"]].age = person["age"]
-            continue
+        if person.get("wife"):
+            wife = Person.people[person["wife"]]
+            husband = Person.people[person["name"]]
+        elif person.get("husband"):
+            husband = Person.people[person["husband"]]
+            wife = Person.people[person["name"]]
+        if wife and husband:
+            wife.husband = husband
+            husband.wife = wife
 
-        person_object = Person(person["name"], person["age"])
-        if person.get("husband"):
-            husband_name = person.get("husband")
-            if husband_name in Person.people:
-                husband_object = Person.people[husband_name]
-            else:
-                husband_object = Person(husband_name, 0)
-            person_object.husband = husband_object
-            husband_object.wife = person_object
-        elif person.get("wife"):
-            wife_name = person.get("wife")
-            if wife_name in Person.people:
-                wife_object = Person.people[wife_name]
-            else:
-                wife_object = Person(wife_name, 0)
-            person_object.wife = wife_object
-            wife_object.husband = person_object
-    return list(Person.people.values())
+    return [Person.people[person["name"]] for person in people]
