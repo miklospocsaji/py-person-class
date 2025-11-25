adfb7e707779e2f40cfcb55e0dfb6930e93e5d0a
miklospocsaji <miklos.pocsaji@gmail.com>
2025-11-25 10:12:14 +0100

    fixes

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ class Person:
 
     people: dict = {}
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[self.name] = self
@@ -11,23 +11,25 @@ class Person:
 def create_person_list(people: list) -> list:
     for person in people:
         # If already exists, skip creation but update age
-        if person['name'] in Person.people:
-            Person.people[person['name']].age = person['age']
+        if person["name"] in Person.people:
+            Person.people[person["name"]].age = person["age"]
             continue
 
-        person_object = Person(person['name'], person['age'])
-        if person.husband:
-            husband_name = person['husband']
+        person_object = Person(person["name"], person["age"])
+        if person.get("husband"):
+            husband_name = person.get("husband")
             if husband_name in Person.people:
                 husband_object = Person.people[husband_name]
             else:
-                husband_object = Person(husband_name, 0)  # Default age 0 if not provided
+                husband_object = Person(husband_name, 0)
             person_object.husband = husband_object
-        elif person.wife:
-            wife_name = person['wife']
+            husband_object.wife = person_object
+        elif person.get("wife"):
+            wife_name = person.get("wife")
             if wife_name in Person.people:
                 wife_object = Person.people[wife_name]
             else:
-                wife_object = Person(wife_name, 0)  # Default age 0 if not provided
+                wife_object = Person(wife_name, 0)
             person_object.wife = wife_object
+            wife_object.husband = person_object
     return list(Person.people.values())
